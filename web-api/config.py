57f29935ca1be2eb5f14e86d4cca1281ec9e9ab9
Isabel Montero <isabelmonterob@gmail.com>
2025-02-11 15:23:45 -0500

    view script and migration updates

---
+++ web-api/config.py
@@ -0,0 +1,6 @@
+DB_CONFIG = {
+    "host": "localhost",
+    "database": "globant_db",
+    "user": "postgres",
+    "password": "imontero"
+}
--- web-api/migration.py
+++ web-api/migration.py
@@ -1,33 +1,23 @@
 import psycopg2
 import csv
-from io import StringIO
+from config import DB_CONFIG
 
-# Datos de conexión
-DB_CONFIG = {
-    "host": "localhost",
-    "database": "globant_db",
-    "user": "postgres",
-    "password": "imontero"
-}
-
-# Cargar CSV a PostgreSQL
 def csv_to_postgres(path, table):
     try:
         with psycopg2.connect(**DB_CONFIG) as con:
             with con.cursor() as cur:
                 with open(path, 'r') as csv_file:
                     reader = csv.reader(csv_file)
-                    #headers = next(reader)  # Leer encabezados
                     
-                    # Convertir CSV a formato compatible con `copy_from`
-                    csv_data = StringIO()
                     for row in reader:
-                        csv_data.write(",".join(row) + "\n")
-                    
-                    csv_data.seek(0)  # Volver al inicio del buffer
+                        if "" in row:
+                            continue
 
-                    # Ejecutar inserción en la tabla
-                    cur.copy_from(csv_data, table, sep=",")
+                        placeholders = ", ".join(["%s"] * len(row))  # Genera '%s, %s, %s' según la cantidad de columnas
+                        
+                        query = f"INSERT INTO {table} VALUES ({placeholders})"
+                        #print (query, row)
+                        cur.execute(query, row)  # Insertar fila en la base de datos
 
                 con.commit()
                 print(f"Datos insertados correctamente en '{table}'.")
@@ -35,7 +25,7 @@ def csv_to_postgres(path, table):
     except Exception as e:
         print(f"Error al insertar datos en '{table}': {e}")
 
-# Uso de la función
+#Insersión de datos de los csv
 csv_to_postgres("sources\jobs.csv", "jobs")
 csv_to_postgres("sources\departments.csv", "departments")
 csv_to_postgres("sources\hired_employees.csv", "hired_employees")
